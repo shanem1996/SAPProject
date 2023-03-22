57db3175a974d018bb6113c99652f07b02db2c2d
shanem1996 <56543705+shanem1996@users.noreply.github.com>
2023-03-22 16:09:20 +0000

    created database

--- app/src/main/java/com/example/sapproject/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/sapproject/activities/SignUpActivity.kt
@@ -1,8 +1,13 @@
 package com.example.sapproject.activities
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.sapproject.activities.database.DatabaseHelper
 import com.example.sapproject.databinding.ActivitySignUpBinding
+import com.google.android.material.snackbar.Snackbar
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -13,5 +18,31 @@ class SignUpActivity : AppCompatActivity() {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val db = DatabaseHelper(this).writableDatabase
+
+        binding.btnSignUp.setOnClickListener {
+            val name = binding.tfName.text.toString()
+            val username = binding.tfUsername.text.toString()
+            val password = binding.tfPassword.text.toString()
+
+            val values = ContentValues()
+            values.put(DatabaseHelper.COLUMN_NAME, name)
+            values.put(DatabaseHelper.COLUMN_USERNAME, username)
+            values.put(DatabaseHelper.COLUMN_PASSWORD, password)
+
+            val newRowId = db.insert(DatabaseHelper.TABLE_NAME, null, values)
+            if (newRowId == -1L) {
+                Snackbar.make(view, "Error has occurred", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, "Success", Snackbar.LENGTH_LONG).show()
+                val home = Intent(this, HomeActivity::class.java)
+                startActivity(home)
+
+            }
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/sapproject/activities/database/Database.kt
+++ app/src/main/java/com/example/sapproject/activities/database/Database.kt
@@ -4,14 +4,25 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-abstract class myDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object{
         private const val DATABASE_NAME = "database.db"
         private const val DATABASE_VERSION = 1
+        const val TABLE_NAME = "users"
+        const val COLUMN_ID = "id"
+        const val COLUMN_NAME = "name"
+        const val COLUMN_USERNAME = "username"
+        const val COLUMN_PASSWORD = "password"
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)")
+        val createTable = ("CREATE TABLE " + TABLE_NAME + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME + " TEXT," + COLUMN_USERNAME + " TEXT," + COLUMN_PASSWORD + " TEXT" + ")")
+        db.execSQL(createTable)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
     }
 
 
